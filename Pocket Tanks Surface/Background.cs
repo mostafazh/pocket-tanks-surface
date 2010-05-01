7afaed6756a53573ce35d14cbe3d61317b3aef47
mostafa.zhosney@gmail.com <mostafa.zhosney@gmail.com@8d060c8e-3141-183b-6cce-b683309cdaf5>
2010-05-01 22:39:43 +0000

    Added the Player Class and the Background Class Added a background to the view

---
+++ Pocket Tanks Surface/Background.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Pocket_Tanks_Surface
+{
+    public class Background
+    {
+        // Background texture
+        private Texture2D Texture;
+        private int Width;
+        private int Height;
+
+        public Background(Texture2D Texture, int Width, int Height)
+        {
+            this.Texture = Texture;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw background texture in a separate pass.
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, new Rectangle(0, 0, Width,Height),Color.LightGray);
+            spriteBatch.End();
+        }
+
+        public void Unload()
+        {
+            Texture.Dispose();
+        }
+    }
+}
